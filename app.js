da6024ceb35dc5fac8c0990b1b135ca16e10f37f
somiaelshemy <somiaelshemy22@gmail.com>
2025-09-01 12:46:20 +0300

    fix: stage flow

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const spotifyController = require('./apis/spotify');
+const youtubeController = require('./apis/ytmusic');
 const app = express();
 
 app.use(express.json());
 app.use(cors()).use(cookieParser());
 
 app.get('/', spotifyController.startScript);
+app.get('/ytauth', youtubeController.ytAuth);
 app.get('/callback', spotifyController.handleCallback);
-
+app.get('/oauth2callback', youtubeController.handleCallback);
 module.exports = app;
